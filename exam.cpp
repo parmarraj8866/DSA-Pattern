47aaab86bb7c12c3cda346ded061c53e11cc4d31
Parmar Raj <rajparmar112005@gmail.com>
2025-01-04 15:19:00 +0530

    Add files via upload

---
+++ exam.cpp
@@ -0,0 +1,139 @@
+//  Qua  ( 1 )
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+    
+//    int start = 41;
+//     for (int i = 1; i <= 5; i++) {
+//         for (int j = 0; j < i; j++) {
+//             cout << start + j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+
+
+//  Qua  ( 2 )
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int num = 11;
+//     for (int i = 1; i < 5; i++) {
+//         for (int j = 0; j < i; j++) {
+//             cout << num << " ";
+//             num++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+
+
+
+//  Qua  ( 3 )
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+// for (int i = 5; i >= 1; i--) {
+//         for (int j = 0; j < i; j++) {
+//             cout << (j % 2) << " ";
+//         }
+//         cout << endl;
+//     }
+//     }
+
+
+
+
+//  Qua  ( 4 )
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     int n = 5; 
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = i; j < n; j++) {
+//             cout << " "; 
+//         }
+
+//         for (int j = n - i + 1; j <= n; j++) {
+//             cout << j;
+//         }
+
+//         for (int j = n - 1; j >= n - i + 1; j--) {
+//             cout << j;
+//         }
+
+//         cout << endl; 
+//     }
+// }
+
+
+ 
+
+
+//   Qua  ( 5 )
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 5;  
+   
+//     for (int i = 1; i <= n; i++) {
+//         cout << i;
+//     }
+    
+//     for (int i = 6 - 1; i >= 1; i--) {
+//         cout << i;
+//     }
+
+//     cout << endl;  
+
+// }
+
+
+
+
+//   Qua  ( 6 )
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 5;  
+
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 1; j <= i; j++) {
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+
+//     for (int i = n - 1; i >= 1; i--) {
+//         for (int j = 1; j <= i; j++) {
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+
+// }
+
+
